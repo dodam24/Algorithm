696caa0733a5062ebf761de072e76277ba5ab81a
Dodam <dodam0724@gmail.com>
2023-08-18 16:38:10 +0900

    [level 0] Title: 모스부호 (1), Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,40 @@
+const morse = {
+    '.-':'a',
+    '-...':'b',
+    '-.-.':'c',
+    '-..':'d',
+    '.':'e',
+    '..-.':'f',
+    '--.':'g',
+    '....':'h',
+    '..':'i',
+    '.---':'j',
+    '-.-':'k',
+    '.-..':'l',
+    '--':'m',
+    '-.':'n',
+    '---':'o',
+    '.--.':'p',
+    '--.-':'q',
+    '.-.':'r',
+    '...':'s',
+    '-':'t',
+    '..-':'u',
+    '...-':'v',
+    '.--':'w',
+    '-..-':'x',
+    '-.--':'y',
+    '--..':'z',
+};
+
+function solution(letter) {
+    
+    let answer = ''
+    const splittedLetter = letter.split(' ');
+    
+    for(let i=0; i < splittedLetter.length; i++) {
+        answer += morse[splittedLetter[i]];
+    }
+    return answer;
+
+}
