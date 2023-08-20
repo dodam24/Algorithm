92a1669633faf1047a1ae60d3ce84566d99b7b0f
Dodam <dodam0724@gmail.com>
2023-08-20 21:59:44 +0900

    [level 0] Title: 합성수 찾기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
@@ -1,14 +1,19 @@
+const getIsCount = (i) => {
+    let isCount = false;
+    for(let num = 2; num < i-1; num++) {
+        if(i % num === 0) {
+            // 합성수
+            isCount = true;
+        }
+    }
+    return isCount;
+}
+
 function solution(n) {
     let answer = 0;
     for(let i = n; i >= 1; i--) {
         // i는 합성수인지 비교해야 하는 숫자
-        let isCount = false;
-        for(let num = 2; num < i-1; num++) {
-            if(i % num === 0) {
-                // 합성수
-                isCount = true;
-            }
-        }
+        const isCount = getIsCount(i);
         // isCount로 합성수 여부를 확인
         if(isCount) {
             answer += 1;
