764541cf7e5d34a122da8f6a48c96bde0593e1aa
Dodam <dodam0724@gmail.com>
2023-08-23 21:12:41 +0900

    [level 0] Title: 숫자 찾기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
@@ -0,0 +1,12 @@
+function solution(num, k) {
+    
+    const strNum = String(num);
+    for(let i=0; i < strNum.length; i++) {
+        const item = strNum[i];
+        if(item === String(k)) {
+            return i + 1;
+        }
+    }
+    
+    return -1;
+}
