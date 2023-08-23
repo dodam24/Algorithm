f7edf41f521d116f511de6d09276e0ac8725e791
Dodam <dodam0724@gmail.com>
2023-08-23 22:28:08 +0900

    Update OX퀴즈.js

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
@@ -9,23 +9,26 @@ function solution(quiz) {
         const secNum = Number(itemArr[2]);
         const result = Number(itemArr[4]);
         
-        if(op === "+") {
-            if(fstNum + secNum === result) {
-                answer.push("O");
-            } else {
-                answer.push("X");
-            }
+        const calResult = op === '+' ? fstNum + secNum : fstNum - secNum;
+
+        if(calResult === result) {
+            answer.push("O");
         } else {
-            if(fstNum - secNum === result) {
-                answer.push("O");
-            } else {
-                answer.push("X");
-            }
+            answer.push("X");
         }
     }
     
     return answer;
 }
+
+// 1. quiz 배열의 원소를 하나씩 꺼낸다.
+// 2. 그 원소를 ' '으로 나눠서 숫자와 연산자를 구분한다.
+// " 3 - 4 = -3" => ["3", "-", "4", "=", "-3"]
+// arr[0] : 첫 번째 숫자
+// arr[1] : 연산자
+// arr[2] : 두 번째 숫자
+// arr[4] : 계산 결과
+// 3. 실제로 계산해보고 값이 맞으면 O를, 아니면 X를 answer에 넣는다.
 
 // 1. quiz 배열의 원소를 하나씩 꺼낸다.
 // 2. 그 원소를 ' '으로 나눠서 숫자와 연산자를 구분한다.
