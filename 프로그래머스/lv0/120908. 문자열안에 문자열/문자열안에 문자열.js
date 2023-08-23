5823287b9a2fc5d2d0a9cf29dc9a7d4bba406932
Dodam <dodam0724@gmail.com>
2023-08-23 23:54:18 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.js
@@ -0,0 +1,20 @@
+// function solution(str1, str2) {
+//     return str1.includes(str2) ? 1 : 2;
+// }
+
+function solution(str1, str2) {
+    
+    for(let str1Idx = 0; str1Idx < str1.length; str1Idx++) {
+        const str1Item = str1[str1Idx];
+        let isAllEqual = true;
+        for(let str2Idx = 0; str2Idx < str2.length; str2Idx++) {
+            if(str1[str1Idx+str2Idx] !== str2[str2Idx]) {
+                isAllEqual = false;
+            }
+        }
+        if(isAllEqual) {
+            return 1;
+        }
+    }
+    return 2;
+}
