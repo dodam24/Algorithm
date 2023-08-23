924b6e918a9afde207bfb5df930742146dee955b
Dodam <dodam0724@gmail.com>
2023-08-23 23:57:17 +0900

    Update 문자열안에 문자열.js

--- 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.js
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.js
@@ -7,9 +7,12 @@ function solution(str1, str2) {
     for(let str1Idx = 0; str1Idx < str1.length; str1Idx++) {
         const str1Item = str1[str1Idx];
         let isAllEqual = true;
+        
         for(let str2Idx = 0; str2Idx < str2.length; str2Idx++) {
             if(str1[str1Idx+str2Idx] !== str2[str2Idx]) {
                 isAllEqual = false;
+                str1Idx = str1Idx + str2Idx;
+                break;
             }
         }
         if(isAllEqual) {
