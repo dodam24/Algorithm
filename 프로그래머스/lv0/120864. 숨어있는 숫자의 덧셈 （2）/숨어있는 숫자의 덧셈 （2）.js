bfe93bb5e638a8b0272f681621fa4d227a443954
Dodam <dodam0724@gmail.com>
2023-08-25 14:00:19 +0900

    Update 숨어있는 숫자의 덧셈 （2）.js

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -11,3 +11,8 @@ function solution(my_string) {
     }
     return result;
 }
+
+// 문자열에 자연수가 없는 경우 0을 return
+// Number.isNaN 함수는 인수가 Number type이 아닌 경우, 그 값을 숫자로 강제
+// tmp에 숫자를 찾으면 하나씩 넣어주고, 붙어있는 숫자를 tmp에 저장
+// tmp에 임시 저장된 문자 형태의 값을 Number(tmp)로 변환 후, result에 더하고 return result를 통해 답을 도출
