9a5534858a03de20374542e5d000bd55c5a0e2ea
Dodam <dodam0724@gmail.com>
2023-08-28 14:03:22 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -0,0 +1,19 @@
+function solution(n) {
+    let answer = 0;
+    
+    for(let i = 1; i <= n; i++) {
+        answer += 1;
+        
+        while(true) {
+            if(answer % 3 === 0 || String(answer).includes("3")) {
+                answer += 1;
+                
+                continue;
+            }
+            
+            break;
+        }
+    }
+    
+    return answer;
+}
