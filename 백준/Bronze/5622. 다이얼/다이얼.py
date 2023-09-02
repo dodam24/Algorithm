5caf262cc71a58cbd6dfa615e87390a09431eab4
Dodam <dodam0724@gmail.com>
2023-09-02 12:42:02 +0900

    [Bronze II] Title: 다이얼, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+alphabet = input()
+dial = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+time = 0
+
+for i in range(len(alphabet)):
+    for j in dial:
+        if alphabet[i] in j:
+            time += dial.index(j) + 3
+
+print(time)
