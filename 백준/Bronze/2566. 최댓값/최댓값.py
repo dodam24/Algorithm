ad1593aacd497e6c9da38bcae00d36bb0c85a7e1
Dodam <dodam0724@gmail.com>
2023-09-04 09:51:40 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+board = []
+
+for _ in range(9) :
+    board.append(list(map(int, input().split())))
+    
+X = 0
+Y = 0
+MAX = -1
+
+for i in range(9) :
+    for j in range(9) :
+        if board[i][j] > MAX :
+            MAX = board[i][j]
+            X = i+1
+            Y = j+1
+            
+print(MAX)
+print(X, Y)
