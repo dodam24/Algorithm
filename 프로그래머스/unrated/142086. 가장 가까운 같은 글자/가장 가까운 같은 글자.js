87d4d47f470da3fbf01f47dca4d06c8ab0419f21
Dodam <dodam0724@gmail.com>
2023-09-20 14:54:29 +0900

    Update 가장 가까운 같은 글자.js

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,4 +1,10 @@
 function solution(s) {
     var answer = [];
+    
+    for(let i =0 ; i< s.length ; i++){
+        const before = s.slice(0,i)    // 현재 글자의 앞 부분까지 자르기
+        ret.push(before.lastIndexOf(s[i]) === -1 ? -1 : i-before.lastIndexOf(s[i]))    // lastIndexOf(): 뒤에서부터 문자열의 위치 찾기
+    }
+    
     return answer;
 }
