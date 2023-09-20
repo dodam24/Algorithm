06ad1678f30200ba93309d1dac3d5174d4edcf32
Dodam <dodam0724@gmail.com>
2023-09-20 13:30:48 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 232 ms, Memory: 55836 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,15 +1,13 @@
 import sys
-
 input = sys.stdin.readline
-
 n, m = map(int, input().split())
 
 dict = {}
 
 for i in range(1, n + 1):
-  a = input().rstrip()
-  dict[i] = a
-  dict[a] = i
+  name = input().rstrip()
+  dict[i] = name
+  dict[name] = i
   
 for i in range(m):
   quest = input().rstrip()
