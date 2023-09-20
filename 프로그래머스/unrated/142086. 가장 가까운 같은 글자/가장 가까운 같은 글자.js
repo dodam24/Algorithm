6e1ca0ca77b92e0355be46767b2f04d9bee46c65
Dodam <dodam0724@gmail.com>
2023-09-20 14:52:04 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 5.74 ms, Memory: 38.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -0,0 +1,9 @@
+function solution(s) {
+    var answer = [];
+    
+    for(let i = 0; i < s.length; i++) {
+        const before = s.slice(0, i)    // 현재 글자의 앞 부분까지 자르기
+        answer.push(before.lastIndexOf(s[i]) === -1 ? -1 : i-before.lastIndexOf(s[i]))    // lastIndexOf(): 뒤에서부터 문자열의 위치 찾기
+        }
+    return answer;
+}
