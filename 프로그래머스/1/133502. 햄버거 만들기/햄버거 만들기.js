2662b510e474e62b780dd3ea3e68149bea431a0c
Dodam <dodam0724@gmail.com>
2023-10-17 09:49:02 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,16 @@
+function solution(ingredient) {
+    let count = 0;
+    
+    for (let i = 0; i < ingredient.length; i++) {
+        if (ingredient.slice(i, i + 4).join('') === '1231') {
+            count++;
+            ingredient.splice(i, 4);
+            i -= 3;
+        }
+    }
+    return count;
+}
+
+// slice(): begin부터 end 전까지의 복사본을 새로운 배열 객체로 반환 (원본 배열 수정 X)
+
+// splice(): 배열의 기존 요소를 삭제 또는 교체하거나 새 요소를 추가하여 배열의 내용을 변경 (원본 배열 자체를 수정) - 시작 인덱스부터 몇 개를 삭제할지
