ee4957cbab7c7503fa50e4318ba7050619706ab6
Dodam <dodam0724@gmail.com>
2023-10-23 16:18:34 +0900

    [level 1] Title: 삼총사, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -0,0 +1,14 @@
+function solution(numbers) {
+  let count = 0;
+
+  for(let i = 0; i < numbers.length; i++) {
+    for(let j = i+1; j < numbers.length; j++) {
+      for(let k = j+1; k < numbers.length; k++) {
+        if(numbers[i] + numbers[j] + numbers[k] === 0) {
+          count += 1;
+        }
+      }
+    }
+  }
+  return count;
+}
