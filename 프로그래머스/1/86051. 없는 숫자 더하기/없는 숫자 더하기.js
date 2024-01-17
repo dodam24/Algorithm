576978f5ca1e5556918230747e280d5e42025f99
Dodam <dodam0724@gmail.com>
2024-01-18 05:55:40 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -9,13 +9,10 @@ function solution(numbers) {
 
 // reduce 함수 사용
 /*
-    function solution(numbers) {
-        var answer = 0;
-        
-        sum = [0, 1, 2, 3].reduce(function (accumulator, currentValue)) {
-            return accumulator + currentValue;
-        }, 0);
-        
-        return answer;
+// 0~9까지의 합인 45에서 .reduce 함수를 적용하여 합산한 배열의 모든 값을 빼준다.
+
+function solution(numbers) {
+	return 45 - numbers.reduce((cur, acc)) => cur + acc, 0);
+}
     }
 */
