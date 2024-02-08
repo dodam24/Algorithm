2d0f28331002f6cda2d3309e9dc441802b378b98
Dodam <dodam0724@gmail.com>
2024-02-08 21:36:28 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
@@ -1,11 +1,10 @@
 function solution(arr) {    
     // return arr.reduce((acc, cur) => acc + cur / arr.length, 0)
    
-    const result = arr.reduce(function add(sum, curVal) {
-        return sum + curVal;
+    const result = arr.reduce(function add(accumulation, currentVal) {
+        return accumulation + currentVal;
     }, 0)
     
     const average = result / arr.length;
-  
     return average;
 }
