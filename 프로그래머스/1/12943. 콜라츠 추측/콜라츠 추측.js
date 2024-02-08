18cf53eb3aa951a07e1ac3b9d2e2b314038ab52f
Dodam <dodam0724@gmail.com>
2024-02-08 21:32:21 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,12 @@
+function solution(num) {
+    if(num === 1) return 0;
+    let count = 1;
+    
+    while(count <= 500) {
+        num = num % 2 === 0 ? num / 2 : num * 3 + 1;
+        
+        if(num === 1) return count;
+        else count += 1;
+    }
+    return -1;
+}
