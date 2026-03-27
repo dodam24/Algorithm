e1aee863d56adb529306737724e04adc9fae12c5
Dodam <dodam0724@gmail.com>
2026-03-27 20:06:32 +0900

    [level 1] Title: 유연근무제, Time: 1.03 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.js
@@ -0,0 +1,25 @@
+function solution(schedules, timelogs, startday) {
+    var answer = 0;
+    
+    function formatTime(time) {
+        let newTime = (Math.floor(time / 100) * 60 + (time % 100)) + 10;
+        return (Math.floor(newTime / 60) * 100 + newTime % 60)
+    }
+    
+    for(let i = 0; i < schedules.length; i++) {
+        let time = formatTime(schedules[i]);
+        let valid = true;
+        
+        for (let j = 0; j < 7; j++) {
+            let dayIdx = (startday + j) % 7;
+            if (dayIdx === 6 || dayIdx === 0) continue;
+            
+            if (timelogs[i][j] > time) {
+                valid = false;
+                break;
+            }
+        }
+        if (valid) answer ++;
+    }
+    return answer;
+}
